573b91c1a96ae0a2c10878a8407f513a84502685
Chris Denmark <chrisdanish@googlemail.com>
2022-06-01 23:06:00 +0100

    Add boss battle and cheatcode

--- eldenring.sh
+++ eldenring.sh
@@ -14,4 +14,23 @@ if [[ $tarnished == $beast ]]; then
 	echo "You slay the beast! Congrats fellow tarnished"
 else
 	echo "You Died, again!"
+	exit 1
+fi
+
+sleep 2
+
+#Boss Battle
+
+echo "Boss Battle. Get scared it's Margit. Pick a number between 0-9. (0-9)"
+
+read tarnished
+
+beast=$(( $RANDOM % 10))
+
+sleep 2
+
+if [[ $tarnished == $beast || $tarnished == "coffee" ]]; then
+	echo "You slayed Margit! Victory is ours"
+else
+	echo "You died, again"
 fi
