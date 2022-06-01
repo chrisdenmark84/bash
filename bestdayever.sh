29a0a569a4db9ddafb5e17fa23a237442462cf40
Chris Denmark <chrisdanish@googlemail.com>
2022-06-01 21:39:10 +0100

    add second argument

--- bestdayever.sh
+++ bestdayever.sh
@@ -1,10 +1,10 @@
 #!bin/bash
 
 name=$1
-
+compliments=$2
 echo "Good Morning $name!!"
 sleep 1
 echo "You're looking good today $name!!"
 sleep 1
-echo "You have the best beard I have ever seen $name!!"
+echo "You have the best $compliments I have ever seen $name!!"
 
