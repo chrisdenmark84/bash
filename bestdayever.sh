111b4e550776c58162e98a3de022459da6cad782
Chris Denmark <chrisdanish@googlemail.com>
2022-06-01 21:52:08 +0100

    add username, pwd and date

--- bestdayever.sh
+++ bestdayever.sh
@@ -2,9 +2,15 @@
 
 name=$1
 compliments=$2
+
+user=$(whoami)
+date=$(date)
+pwd=$(pwd)
+
 echo "Good Morning $name!!"
 sleep 1
 echo "You're looking good today $name!!"
 sleep 1
 echo "You have the best $compliments I have ever seen $name!!"
-
+sleep 2
+echo "You are currently logged in as $user and you are in the directory $pwd. Today is $date"
